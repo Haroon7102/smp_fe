1246fe4bb63c3a75aee9a714b2a21b37faf8ce77
haroon 7012 <70120821@student.uol.edu.pk>
2024-10-30 16:52:29 +0500

    after making changes

--- src/Components/InstagramLoginCheck.js
+++ src/Components/InstagramLoginCheck.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const InstagramLoginButton = () => {
-    const instagramAuthURL = "https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1199616704485910&redirect_uri= https://smpfe.netlify.app/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish";
+    const instagramAuthURL = "https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1199616704485910&redirect_uri=https://smpfe.netlify.app/auth/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish";
 
     return (
         <a href={instagramAuthURL} className="btn btn-primary">
